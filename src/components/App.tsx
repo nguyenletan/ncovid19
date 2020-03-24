e00e088056374b731e75088fe12a7127f6fc5828
nguyenletan <nguyenletan@gmail.com>
2020-03-24 21:04:00 +1100

    Removed country box

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import CountryPicker from './CountryPicker';
+/*import CountryPicker from './CountryPicker';*/
 import Hero from './Hero';
 import Maps from './Maps';
 import Stats from './Stats';
